edd2e04495c0b5f7f4af16805a7b328acccec6fd
GetAlexed0 <71035944+GetAlexed0@users.noreply.github.com>
2021-06-05 16:51:38 +0200

    Fixed refactoring

--- app/src/main/java/com/example/myeverest/ui/StartingActivity.java
+++ app/src/main/java/com/example/myeverest/ui/StartingActivity.java
@@ -5,8 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import Login;
 import com.example.myeverest.MainActivity;
+import com.example.myeverest.User.Login;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class StartingActivity extends AppCompatActivity {
